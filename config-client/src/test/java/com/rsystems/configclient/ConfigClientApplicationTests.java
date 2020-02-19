265ac726f5dee7096c3cbba910b3d367133b0296
Mohammad Adnan <Mohammad.Adnan@rsystems.com>
2020-02-19 17:18:26 +0530

    initial commit

---
+++ config-client/src/test/java/com/rsystems/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsystems.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
